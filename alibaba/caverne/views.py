4c3a81859aaf7f0273614a98a668ce1ccffead0c
Ali Ibrahim <ali.developer14@gmail.com>
2026-02-24 12:39:09 +0100

    (project) Made a test user to be used for the moment

--- alibaba/caverne/views.py
+++ alibaba/caverne/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpResponse
 from .forms import FichierForm
-
+from .models import Fichier, User
 # Create your views here.
 def index(request):
     return render(request, "caverne/index.html")
 
 def upload(request):
+    testuser = User.objects.get(username="test")
     if request.method == "POST":
         form = FichierForm(request.POST, request.FILES)
         if form.is_valid():
             form = form.save(commit=False)
-            form.user = request.user
+            form.user = testuser
             form.save()
             return redirect(reverse('index'))
     else:
