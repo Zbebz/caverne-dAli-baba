4a5df03bda0c0ea4f0e98a786d40b89111d31821
Ali Ibrahim <ali.developer14@gmail.com>
2026-03-17 23:10:13 +0100

    (project) Made the 2nd step of the upload wireframe

--- alibaba/caverne/forms.py
+++ alibaba/caverne/forms.py
@@ -7,18 +7,28 @@ class FichierForm(forms.ModelForm):
 
     class Meta:
         model = Fichier
-        exclude = ['user', 'uploadDatetime', 'status']
+        exclude = ["user", "uploadDatetime", "status"]
         widgets = {
-            'file': forms.FileInput(attrs={'hidden': 'true'}),
+            "file": forms.FileInput(attrs={"hidden": "true"}),
+            "annotated": forms.CheckboxInput(attrs={"class": "form-check-input"}),
         }
 
     def __init__(self, *args, **kwargs):
         super(FichierForm, self).__init__(*args, **kwargs)
 
-        for field_name, field in self.fields.items():
-            if hasattr(self.fields[field_name], "choices"):
-                l = list(self.fields[field_name].choices)[1:]
-                print(l, flush=True)
-                self.fields[field_name].choices = [
+        for field in self.fields.values():
+            if hasattr(field, "choices"):
+                attributes = {"class": "form-select"}
+                field.widget.attrs.update(attributes)
+                choices = list(field.choices)[1:]
+                field.choices = [
                     ("", f"Choisissez un.e {field.label.lower()}")
-                ] + l
+                ] + choices
+                
+            attributes = {"class": "form-control"}
+            for attr in attributes:
+                attributes[attr] = field.widget.attrs.get(
+                    attr, attributes[attr]
+                )
+                field.widget.attrs.update(attributes)
+                attributes = {"class": "form-control"}
