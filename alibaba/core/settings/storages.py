67a67085500ce7a9a2826adbf5ad75fe8256fbf7
Ali Ibrahim <ali.developer14@gmail.com>
2026-05-29 14:24:06 +0200

    (project) Fixed s3 acl bug

--- alibaba/core/settings/storages.py
+++ alibaba/core/settings/storages.py
@@ -3,13 +3,11 @@ from storages.backends.s3boto3 import S3Boto3Storage
 
 class StaticStorage(S3Boto3Storage):
     location = "static"
-    default_acl = "public-read"
     querystring_auth = False
 
 
 class MediaStorage(S3Boto3Storage):
     location = "media"
     file_overwrite = False
-    default_acl = "private"
     querystring_auth = True
     querystring_expire = 600
